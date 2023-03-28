f27cb65f5fab4ac577af30e57bbd9c3a954c614f
DixonMch <dixonmogaka@gmail.com>
2023-03-28 22:52:50 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskInput = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // To prevent the form from submitting normally
+
+    const taskText = taskInput.value.trim(); // To get the task description from the input field
+    if (taskText) {
+      // To create a new list item for the task and append it to the list
+      const taskItem = document.createElement("li");
+      taskItem.innerText = taskText;
+      taskList.appendChild(taskItem);
+
+      // To create a delete button for the task and append it to the item
+      const deleteButton = document.createElement("button");
+      deleteButton.innerText = "x";
+      deleteButton.addEventListener("click", () => {
+        taskItem.remove(); // To remove the task item when the delete button is clicked
+      });
+      taskItem.appendChild(deleteButton);
+
+      taskInput.value = ""; // To clear the input field
+    }
+  });
 });
